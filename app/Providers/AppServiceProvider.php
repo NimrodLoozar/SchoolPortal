8e74ef1c8f35add0343a389d9f5fb62acde6605a
NimrodLoozar <nimrod.lobozarl.com>
2025-07-03 22:46:49 +0200

    Add theme toggle component and functionality; update navigation and routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Auth\ContactEmailUserProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use App\View\Components\ThemeToggle;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
         Auth::provider('contact_email', function ($app, array $config) {
             return new ContactEmailUserProvider();
         });
+        Blade::component('theme-toggle', ThemeToggle::class);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,41 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html"
     ],
-
+    darkMode: 'class',
     theme: {
         extend: {
+            colors: {
+                primary: '#3B82F6',
+                secondary: '#64748B',
+                accent: '#FBBF24',
+                neutral: '#374151',
+                'base-100': '#FFFFFF',
+                info: '#3ABFF8',
+                success: '#36D399',
+                warning: '#FBBD23',
+                error: '#F87272',
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
-
-    plugins: [forms],
+    variants: {
+        extend: {
+            backgroundColor: ['active'],
+            textColor: ['active'],
+            borderColor: ['active'],
+        },
+    },
+    plugins: [],
 };
